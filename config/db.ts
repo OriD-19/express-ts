88996f4b02225005399534cb5831d2594ceec413
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-03-18 19:11:48 -0600

    Fixed Database Connection String

--- config/db.ts
+++ config/db.ts
@@ -6,7 +6,7 @@ export const connectDB = async () => {
     const username = process.env.DB_USERNAME;
     const password = process.env.DB_PASSWORD;
 
-    const url = `mongodb+srv://<${username}>:<${password}>@backend-db.4dyny.mongodb.net/?retryWrites=true&w=majority&appName=backend-db`;
+    const url = `mongodb+srv://${username}:${password}@backend-db.4dyny.mongodb.net/?retryWrites=true&w=majority&appName=backend-db`;
     try {
         const connection = await mongoose.connect(url);
     } catch (err: any) {
